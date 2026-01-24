5d2f18c6091c993e98e8b4d93636321214a11992
Johanna <yellowyocat@gmail.com>
2026-01-24 12:14:09 -0700

    update

--- mavjo/src/app/page.tsx
+++ mavjo/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Layout from '../app/layout';
+import HomeComponent from '../app/home/page'
 
 
 export default function Home() {
   return (
-    <h1></h1>
+   <HomeComponent></HomeComponent>
   );
 }
 
