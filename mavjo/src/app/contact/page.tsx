7b6a98bc76b4275c8ce0ccc699b0225343a99b66
Johanna <yellowyocat@gmail.com>
2026-01-31 20:17:29 -0700

    contact fixed

--- mavjo/src/app/contact/page.tsx
+++ mavjo/src/app/contact/page.tsx
@@ -24,79 +24,82 @@ const page = () => {
 
 
 
-      <div className="hudContainer">
 
-        {/* Decorative HUD Images */}
-        <Image
-          src="/imgs/topcorner.png"
-          alt=""
-          width={500}
-          height={60}
-          className="hudTop"
-          priority
-        />
+      <div className='contact-form'>
+        <div className="hudContainer">
 
-        <Image
-          src="/imgs/line.png"
-          alt=""
-          width={500}
-          height={60}
-          className="hudBottom"
-        />
+          {/* Decorative HUD Images */}
+          <Image
+            src="/imgs/topcorner.png"
+            alt=""
+            width={500}
+            height={60}
+            className="hudTop"
+            priority
+          />
 
-        <Image
-          src="/imgs/rightline.png"
-          alt=""
-          width={60}
-          height={400}
-          className="hudLeft"
-        />
+          <Image
+            src="/imgs/line.png"
+            alt=""
+            width={500}
+            height={60}
+            className="hudBottom"
+          />
 
-        <Image
-          src="/imgs/leftline.png"
-          alt=""
-          width={60}
-          height={400}
-          className="hudRight"
-        />
+          <Image
+            src="/imgs/rightline.png"
+            alt=""
+            width={60}
+            height={400}
+            className="hudLeft"
+          />
 
-        {/* Actual Form */}
-        <div className="contactWrapper">
-          <h2>Contact Us</h2>
+          <Image
+            src="/imgs/leftline.png"
+            alt=""
+            width={60}
+            height={400}
+            className="hudRight"
+          />
 
-          <form className="contactForm">
-            <label>
-              <span>Name</span>
-              <input type="text" />
-            </label>
+          {/* Actual Form */}
+          <div className="contactWrapper">
+            <h2>Contact Us</h2>
 
-            <label>
-              <span>Email</span>
-              <input type="email" />
-            </label>
+            <form className="contactForm">
+              <label>
+                <span>Name</span>
+                <input type="text" />
+              </label>
 
-            <label>
-              <span>Message</span>
-              <textarea />
-            </label>
+              <label>
+                <span>Email</span>
+                <input type="email" />
+              </label>
 
-            <Link href="" className="consultation-link">
-              <div className="consultation-banner">
-                <Image
-                  src="/imgs/btn.png"
-                  alt="Book a Consultation"
-                  width={200}
-                  height={200}
-                  className="banner-image"
-                />
-                <div className="banner-overlay">
-                  <span className="banner-text">Message</span>
+              <label>
+                <span>Message</span>
+                <textarea />
+              </label>
+
+              <Link href="" className="consultation-link">
+                <div className="consultation-banner">
+                  <Image
+                    src="/imgs/btn.png"
+                    alt="Book a Consultation"
+                    width={200}
+                    height={200}
+                    className="banner-image"
+                  />
+                  <div className="banner-overlay">
+                    <span className="banner-text">Message</span>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </form>
-        </div>
+              </Link>
+            </form>
+          </div>
 
+        </div>
       </div>
     </main>
   )
